f8de280384c417135f43a68dd21af38fade4a5ae
yanyanyansndd <396987602@qq.com>
2021-10-08 20:57:54 +0800

    test1

---
+++ src/test1/ShowLogicErrors.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class ShowLogicErrors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("Celsius 35 is Fahrenheit degree ");
+        System.out.println((9.0 / 5) * 35 + 32);//9.0²»ÊÇ9
+	}
+
+}
---
+++ src/test1/ShowRuntimeErrors.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class ShowRuntimeErrors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println(1 / 0);//0不能作为除数
+	}
+
+}
